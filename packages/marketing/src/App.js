49456be8583fa4f0eaf59bd34730f9fb5e4d34aa
160119 <xiaoyi.du@uts.edu.au>
2023-06-16 10:27:38 +1000

    first commit

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core/styles";
-import Landing from "./components/Landing";
-import Pricing from "./components/Pricing";
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
 
 export default () => {
   return (
@@ -11,7 +12,7 @@ export default () => {
         <BrowserRouter>
           <Switch>
             <Route exact path="/pricing" component={Pricing} />
-            <Route exact path="/" component={Landing} />
+            <Route path="/" component={Landing} />
           </Switch>
         </BrowserRouter>
       </StylesProvider>
