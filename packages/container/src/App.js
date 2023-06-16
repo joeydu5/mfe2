9dbff7aac5b00b7d006e90aa295e321aae2c9fde
160119 <xiaoyi.du@uts.edu.au>
2023-06-16 12:33:37 +1000

    dummy change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
 
 export default () => {
   return (
     <div>
       <h1>Hi there!</h1>
       <hr />
+      <h2>Hi</h2>
       <MarketingApp />
     </div>
   );
