3227f58f44d8e0ce531628f90aaf84600a167840
160119 <xiaoyi.du@uts.edu.au>
2023-06-29 10:28:41 +1000

    container change

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
     <div>
-      <h1>Hi there!</h1>
-      <hr />
+      <Header />
       <MarketingApp />
     </div>
   );
